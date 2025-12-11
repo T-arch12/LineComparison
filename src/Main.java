e13fad829e83a1f1b80d3f5d0a770eadf7877242
T-arch12 <tejasribadala@gmail.com>
2025-12-11 17:42:02 +0530

    useCase2

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,9 @@ class LineComparison {
         // UC1
         Line l1 = new Line(new Point(1, 2), new Point(4, 6));
         System.out.println("\nUC1: Line Length = " + l1.getLength());
+        //UC2
+        Line l2 = new Line(new Point(0, 0), new Point(3, 4));
+        Line l3 = new Line(new Point(1, 1), new Point(4, 5));
+        System.out.println("\nUC2: l2 equals l3 → " + l2.equals(l3));
     }
 }
